bf0b562575f3803861b2646bdac3e4d79ebd3f35
jannes <jannessantoso@gmail.com>
2016-02-02 17:40:46 +0700

    kelupaan

--- boot/config/routes.js
+++ boot/config/routes.js
@@ -31,4 +31,7 @@ exports.rerute = function(app,path_controller,csrf,bodyParser,passport){
   
   var share = require(path_controller+'share');
   app.get('/app/v3/invitelink',share.index);
+  
+  var commerce = require(path_controller+'commerce');
+  app.get('/app/v3/product/list/:event_id',commerce.index);
 }
