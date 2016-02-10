57ef1637704f635a2f6108e3a49d0873b3cb266f
jannes <jannessantoso@gmail.com>
2016-02-10 16:56:29 +0700

    update commerce summary

--- commerce/scheme/order_product.js
+++ commerce/scheme/order_product.js
@@ -5,17 +5,19 @@ ObjectId = Schema.ObjectId;
 var varScheme = new Schema({
 	// id : {type:Number,required:true,index: {unique: true, dropDups: true}, default: },
 	
-	ticket_id : {type:String, default:''},
-	event_id : {type:String, default:''},
-	name : {type:String, default:''},
-	ticket_type : {type:String, default:''},
-	quantity : {type:Number, default:0},
-	total_price : {type:Number, default:0},
-	
-	num_buy : {type:Number, default:0},
-	total_price_all : {type:Number, default:0},
-	fb_id : {type:String,required:true},
-	// bio   :  { type: String, match: /[a-z]/ },
+	code : {type:String, min:12, max:12},
+	status : {type:String, enum:['summary','confirmation','unpaid','paid','cancel']},
+	product_list : [{
+		ticket_type : {type:String, enum:['purchase','reservation']},
+		num_buy : {type:String, default:'0'},
+		total_price : {type:String, default:'0'},
+		name:{type:String,default:'n'},
+		ticket_id:{type:String,default:'n'},
+		total_price_all:{type:String,default:'0'}
+	}],
+	fb_id:{type:String,required:true},
+	event_id:{type:String,required:true},
+	total_price:{type:String,required:true},
 	created_at  :  { type: Date, default: Date.now }
 });
 
