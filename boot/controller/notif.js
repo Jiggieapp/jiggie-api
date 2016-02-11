32d1563cc5e370317e7e3b589ffe4b3038434264
jannes <jannessantoso@gmail.com>
2016-02-11 15:42:03 +0700

    open push notif

---
+++ boot/controller/notif.js
@@ -0,0 +1,19 @@
+var async = require('async');
+var url = "http://127.0.0.1:16523";
+var curl = require('request');
+
+exports.index = function(req,res){
+	var post = req.body;
+	var options = {
+		url : url+"/apn",
+		form : post
+	}
+	curl.post(options,function(err,resp,body){
+		if (!err && resp.statusCode == 200) {
+			res.header("Content-type","application/json");
+			res.send(body);
+		}else{
+			res.send(err);
+		}
+	})
+}
