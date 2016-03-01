0db05eeba5c2712521a345f893a3614d126b6c8e
jannes <jannessantoso@gmail.com>
2016-03-01 14:53:30 +0700

    update

--- boot/config/routes.js
+++ boot/config/routes.js
@@ -45,6 +45,7 @@ exports.rerute = function(app,path_controller,csrf,bodyParser,passport){
   app.get('/app/v3/product/list/:event_id',commerce.index);
   app.post('/app/v3/product/summary',commerce.post_summary);
   app.get('/app/v3/product/term/:codeid',commerce.term);
+  app.post('/app/v3/product/payment',commerce.payment);
   
   var xmpp = require(path_controller+'xmpp');
   app.get('/xmpp/:user',xmpp.index);
