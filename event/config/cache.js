a737dabb04d54b7211cbf1a6fbad3bc387d345b8
jannes <jannessantoso@gmail.com>
2016-03-04 17:33:34 +0700

    update event

--- event/config/cache.js
+++ event/config/cache.js
@@ -1,4 +1,4 @@
 var NodeCache = require("node-cache");
-var cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
+var cache = new NodeCache({ stdTTL: 20, checkperiod: 20 });
 
 module.exports = cache;
